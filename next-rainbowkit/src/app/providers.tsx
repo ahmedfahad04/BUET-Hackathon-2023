cd8ccc48f4d1121de8e28cdf62525e1e04c3db62
ahmedfahad04 <bsse1204@iit.du.ac.bd>
2023-07-14 05:43:18 +0600

    Adding Home page

--- next-rainbowkit/src/app/providers.tsx
+++ next-rainbowkit/src/app/providers.tsx
@@ -7,6 +7,7 @@ import "../utils/global.css";
 
 import Navbar from "../components/Navbar";
 import { chains, config } from "../wagmi";
+import Home from "../components/Home";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const [mounted, setMounted] = React.useState(false);
@@ -16,6 +17,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
       <RainbowKitProvider chains={chains}>
         <Navbar />
         {mounted && children}
+        <Home />
       </RainbowKitProvider>
     </WagmiConfig>
   );
--- next-rainbowkit/src/components/Navbar.tsx
+++ next-rainbowkit/src/components/Navbar.tsx
@@ -3,23 +3,23 @@ import { ConnectButton } from "../components/ConnectButton";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="flex items-center justify-between bg-gray-100 py-4 px-8">
-      <div className="text-xl font-bold hover:text-blue-600">
+    <nav className="flex items-center justify-between bg-gradient-to-r from-purple-500 to-blue-500 py-4 px-8">
+      <div className="text-xl font-bold hover:text-white">
         <Link href="/">
-          Art Platform
+          <a className="text-white">Art Platform</a>
         </Link>
       </div>
       <div className="flex items-center space-x-4">
         <Link href="/supplier">
-          <p className="text-gray-700 hover:text-blue-600 ">Suppliers</p>
+          <a className="text-white hover:text-blue-200">Suppliers</a>
         </Link>
         <Link href="/verifier">
-          <p className="text-gray-700 hover:text-blue-600">Verifiers</p>
+          <a className="text-white hover:text-blue-200">Verifiers</a>
         </Link>
       </div>
-      <button>
+      <div>
         <ConnectButton />
-      </button>
+      </div>
     </nav>
   );
 };
